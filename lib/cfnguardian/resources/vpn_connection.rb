5871143365bd83188a3ef670a8ddb0efd7fd3ab5
Kyle Thorne <k.thorne@base2services.com>
2021-07-26 15:40:06 +1000

    Fixed VPN Connection to alert when one tunnel is down

--- lib/cfnguardian/resources/vpn_connection.rb
+++ lib/cfnguardian/resources/vpn_connection.rb
@@ -3,7 +3,20 @@ module CfnGuardian::Resource
       
       def default_alarms    
         alarm = CfnGuardian::Models::VPNConnectionAlarm.new(@resource)
-        alarm.name = 'VPNConnectionState'
+        alarm.name = 'VPNConnectionStateNonRedundant'
+        alarm.metric_name = 'TunnelState'
+        alarm.comparison_operator = 'LessThanThreshold'
+        alarm.statistic = 'Average'
+        alarm.threshold = 1.0
+        alarm.evaluation_periods = 3
+        alarm.treat_missing_data = 'breaching'
+        alarm.datapoints_to_alarm = 3
+        @alarms.push(alarm)
+      end
+
+      def default_alarms    
+        alarm = CfnGuardian::Models::VPNConnectionAlarm.new(@resource)
+        alarm.name = 'VPNConnectionStateAllDown'
         alarm.metric_name = 'TunnelState'
         alarm.comparison_operator = 'LessThanThreshold'
         alarm.statistic = 'Average'
