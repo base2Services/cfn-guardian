1c574a9f61c4f97259cdbff3f538d057cebf1028
Samseppiol <j_hage@live.com>
2021-10-14 15:02:51 +1100

    Add jenkins synthetic alarms

---
+++ lib/cfnguardian/resources/jenkins.rb
@@ -0,0 +1,17 @@
+module CfnGuardian::Resource
+    class AmazonMQRabbitMQBroker < Base
+
+        def default_alarms
+            alarm = CfnGuardian::Models::JenkinsAlarm.new(@resource)
+            alarm.name = 'NoSuccess'
+            alarm.metric_name = 'Success'
+            alarm.statistic = 'Maximum'
+            alarm.treat_missing_data = 'breaching'
+            alarm.alarm_action = 'Warning'
+            alarm.period = 3600
+            alarm.comparison_operator = 'LessThanThreshold'
+            alarm.threshold = 1
+            @alarms.push(alarm)
+        end
+    end
+end
