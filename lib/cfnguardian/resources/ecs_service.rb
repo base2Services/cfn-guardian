86cb8d23c3069db051a1591a03da7d4b6a0bc19c
Kyle Thorne <k.thorne@base2services.com>
2021-12-17 16:08:50 +1100

    Added CPU and memory to ECS services, disabled by default

--- lib/cfnguardian/resources/ecs_service.rb
+++ lib/cfnguardian/resources/ecs_service.rb
@@ -25,8 +25,59 @@ module CfnGuardian
         alarm.datapoints_to_alarm = 8
         alarm.alarm_action = 'Warning'
         @alarms.push(alarm)
+
+        alarm = CfnGuardian::Models::ECSServiceAlarm.new(@resource)
+        alarm.name = 'MemoryUtilizationCritical'
+        alarm.metric_name = 'MemoryUtilization'
+        alarm.comparison_operator = 'GreaterThanOrEqualToThreshold'
+        alarm.statistic = 'Average'
+        alarm.threshold = 90
+        alarm.evaluation_periods = 5
+        alarm.treat_missing_data = 'breaching'
+        alarm.alarm_action = 'Critical'
+        alarm.datapoints_to_alarm = 4
+        alarm.enabled = false
+        @alarms.push(alarm)
+        
+        alarm = CfnGuardian::Models::ECSServiceAlarm.new(@resource)
+        alarm.name = 'MemoryUtilizationWarning'
+        alarm.metric_name = 'MemoryUtilization'
+        alarm.comparison_operator = 'GreaterThanOrEqualToThreshold'
+        alarm.statistic = 'Average'
+        alarm.threshold = 80
+        alarm.evaluation_periods = 5
+        alarm.treat_missing_data = 'breaching'
+        alarm.datapoints_to_alarm = 4
+        alarm.alarm_action = 'Warning'
+        alarm.enabled = false
+        @alarms.push(alarm)   
+
+        alarm = CfnGuardian::Models::ECSServiceAlarm.new(@resource)
+        alarm.name = 'CPUUtilizationCritical'
+        alarm.metric_name = 'CPUUtilization'
+        alarm.comparison_operator = 'GreaterThanOrEqualToThreshold'
+        alarm.statistic = 'Average'
+        alarm.threshold = 90
+        alarm.evaluation_periods = 5
+        alarm.treat_missing_data = 'breaching'
+        alarm.alarm_action = 'Critical'
+        alarm.datapoints_to_alarm = 4
+        alarm.enabled = false
+        @alarms.push(alarm)
+        
+        alarm = CfnGuardian::Models::ECSServiceAlarm.new(@resource)
+        alarm.name = 'CPUUtilizationWarning'
+        alarm.metric_name = 'CPUUtilization'
+        alarm.comparison_operator = 'GreaterThanOrEqualToThreshold'
+        alarm.statistic = 'Average'
+        alarm.threshold = 80
+        alarm.evaluation_periods = 5
+        alarm.treat_missing_data = 'breaching'
+        alarm.datapoints_to_alarm = 4
+        alarm.alarm_action = 'Warning'
+        alarm.enabled = false
+        @alarms.push(alarm)      
       end
-      
     end
   end
 end
