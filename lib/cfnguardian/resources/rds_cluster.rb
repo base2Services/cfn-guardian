bbd0b9649231e694d6b4876f79b4445ff0a07c12
Jared Brook <j.brook@base2services.com>
2022-08-24 17:47:48 +1000

    Remove duplicate methods

--- lib/cfnguardian/resources/rds_cluster.rb
+++ lib/cfnguardian/resources/rds_cluster.rb
@@ -7,9 +7,7 @@ module CfnGuardian::Resource
         event_subscription.rds_event_category = 'failover'
         event_subscription.message = 'A failover for the DB cluster has failed.'
         @event_subscriptions.push(event_subscription)
-      end
 
-      def default_event_subscriptions()
         event_subscription = CfnGuardian::Models::RDSClusterEventSubscription.new(@resource)
         event_subscription.name = 'FailoverFinished'
         event_subscription.rds_event_category = 'failover'
