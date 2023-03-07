31739b180bafb7d539c01a2f7f296a62ddd9a8f9
Tarun Menon <tarunmenon95@gmail.com>
2023-03-07 14:47:47 +1100

    Just remove keys with ? char

--- lib/cfnguardian/tagger.rb
+++ lib/cfnguardian/tagger.rb
@@ -27,13 +27,15 @@ module CfnGuardian
 
       if tags_changed?(current_tags, new_tags)
         logger.debug "Updating tags on alarm #{alarm_arn}"
+        new_tags.delete_if {|key, value| value.include?('?')}
         begin
           @client.tag_resource({
             resource_arn: alarm_arn,
             tags: new_tags.map {|key,value| {key: key, value: value}}
           })
         rescue Aws::CloudWatch::Errors::InvalidParameterValue => e
-          logger.debug "Caught invalid character in #{e}"
+          logger.debug "Failed due to invalid character in tags for: #{alarm_arn}"
+        end
       end
     end
 
