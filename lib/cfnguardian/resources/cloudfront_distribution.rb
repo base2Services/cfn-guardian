c1f893060246139e94048b1c03e06cbb3a4f9903
jay verma <jayverma@192-168-1-105.tpgi.com.au>
2023-04-13 12:00:23 +1000

    updated the alarm statistic

--- lib/cfnguardian/resources/cloudfront_distribution.rb
+++ lib/cfnguardian/resources/cloudfront_distribution.rb
@@ -6,11 +6,13 @@ module CfnGuardian::Resource
       alarm.name = '4xxErrorRate'
       alarm.metric_name = '4xxErrorRate'
       alarm.threshold = 10
+      alarm.statistic = 'Average'
       @alarms.push(alarm)
       
       alarm = CfnGuardian::Models::CloudFrontDistributionAlarm.new(@resource)
       alarm.name = '5xxErrorRate'
       alarm.metric_name = '5xxErrorRate'
+      alarm.statistic = 'Average'
       alarm.threshold = 10
       @alarms.push(alarm)
     end
