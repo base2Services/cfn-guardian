79a7ef64a570bf5378218a1598f67f11fe27a742
jay verma <jayverma@192-168-1-105.tpgi.com.au>
2023-04-13 11:09:37 +1000

    Updated 4xx and 5xx error rate alarm and removed total error alarm

--- lib/cfnguardian/resources/cloudfront_distribution.rb
+++ lib/cfnguardian/resources/cloudfront_distribution.rb
@@ -5,20 +5,13 @@ module CfnGuardian::Resource
       alarm = CfnGuardian::Models::CloudFrontDistributionAlarm.new(@resource)
       alarm.name = '4xxErrorRate'
       alarm.metric_name = '4xxErrorRate'
-      alarm.threshold = 2
-      alarm.statistic = 'Sum'
+      alarm.threshold = 10
       @alarms.push(alarm)
       
       alarm = CfnGuardian::Models::CloudFrontDistributionAlarm.new(@resource)
       alarm.name = '5xxErrorRate'
       alarm.metric_name = '5xxErrorRate'
-      alarm.threshold = 5
-      @alarms.push(alarm)
-      
-      alarm = CfnGuardian::Models::CloudFrontDistributionAlarm.new(@resource)
-      alarm.name = 'TotalErrorRate'
-      alarm.metric_name = 'TotalErrorRate'
-      alarm.threshold = 5
+      alarm.threshold = 10
       @alarms.push(alarm)
     end
     
